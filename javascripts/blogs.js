d154808d2cbb35f6df77fdd2a0f0793a2d229d23
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-06-10 20:32:09 -0500

    wip added bootstrap nav

--- javascripts/blogs.js
+++ javascripts/blogs.js
@@ -1,16 +1,16 @@
 const writeToDom = (stringToPrint, divId) => {
-    document.getElementById(divId).innerHTML += stringToPrint;
+  document.getElementById(divId).innerHTML += stringToPrint;
 };
 
 const createBlogPosts = (blogArray) => {
-    for (let i = 0; i < blogArray.length; i++) {
-        let blogString = '';
-        blogString += '<div class="blog-styles">';
-        blogString += '<h2>' + blogArray[i].title + '</h2>';
-        blogString += '<h2>' + blogArray[i].date + '</h2>';
-        blogString += '<p>' + blogArray[i].post + '</p>';
-        blogString += '</div>';
-        writeToDom(blogString, 'blog-container');
+  for (let i = 0; i < blogArray.length; i++) {
+    let blogString = '';
+    blogString += '<div class="blog-styles">';
+    blogString += '<h2>' + blogArray[i].title + '</h2>';
+    blogString += '<h2>' + blogArray[i].date + '</h2>';
+    blogString += '<p>' + blogArray[i].post + '</p>';
+    blogString += '</div>';
+    writeToDom(blogString, 'blog-container');
     }
 };
 
