0f97288f859e769b5cd35bae6f3e664a95753707
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-06-26 11:47:54 -0500

    added styling to projects page

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,11 +1,15 @@
 const projectDom = (projectArray) => {
   let projectString = '';
   projectArray.forEach((project) => {
-    projectString += `<div>`;
-    projectString += `<h1 class="title">${project.title}<h1>`;
+    projectString += `<div class="col-md-4" text-center">`;
+    projectString += `<div class="thumbnail">`;
     projectString += `<img src= "${project.imageUrl}">`;
+    projectString += `<div class="caption">`;
+    projectString += `<h3 class="title text-center">${project.title}<h3>`;
     projectString += `<p>${project.description}</p>`;
-    projectString += `<a href="${project.githubUrl}">Link to Github Profile</a>`;
+    projectString += `<p class="text-center"><a href="${project.githubUrl}" target="_blank" class="btn btn-primary" role="button">Github</a> <a href="${project.url}" class="btn btn-default" target="_blank" role="button">Project</a></p>`;
+    projectString += `</div>`;
+    projectString += `</div>`;
     projectString += `</div>`;
   });
   writeToDom('#projects-container', projectString);
